3990409b4e24ec35fd959512b0e1eb482fb07084
asherpasha <asher.pasha@utoronto.ca>
2025-12-08 19:13:33 -0500

    GAIA publications collection initial commit.

--- api/models/gaia.py
+++ api/models/gaia.py
@@ -22,3 +22,41 @@ class Aliases(db.Model):
     id: db.Mapped[int] = db.mapped_column(db.Integer, nullable=False, primary_key=True)
     genes_id: db.Mapped[int] = db.mapped_column(ForeignKey("genes.id", ondelete="CASCADE"), nullable=False)
     alias: db.Mapped[str] = db.mapped_column(db.String(256), nullable=False)
+
+
+class PublicationFigures(db.Model):
+    __bind_key__ = "gaia"
+    __tablename__ = "publication_figures"
+
+    id: db.Mapped[int] = db.mapped_column(db.Integer, nullable=False, primary_key=True)
+    title: db.Mapped[str] = db.mapped_column(db.String(512), nullable=True)
+    abstract: db.Mapped[str] = db.mapped_column(db.Text, nullable=True)
+    children: db.Mapped[List["PubIds"]] = relationship()
+    children: db.Mapped[List["Figures"]] = relationship()
+
+
+class PubIds(db.Model):
+    __bind_key__ = "gaia"
+    __tablename__ = "pub_ids"
+
+    id: db.Mapped[int] = db.mapped_column(db.Integer, nullable=False, primary_key=True)
+    publication_figures_id: db.Mapped[int] = db.mapped_column(db.Integer, nullable=False)
+    publication_figures_id: db.Mapped[int] = db.mapped_column(
+        ForeignKey("publication_figures.id", ondelete="CASCADE"), nullable=False
+    )
+    pubmed: db.Mapped[str] = db.mapped_column(db.String(16), nullable=True)
+    pmc: db.Mapped[str] = db.mapped_column(db.String(16), nullable=True)
+
+
+class Figures(db.Model):
+    __bind_key__ = "gaia"
+    __tablename__ = "figures"
+
+    id: db.Mapped[int] = db.mapped_column(db.Integer, nullable=False, primary_key=True)
+    publication_figures_id: db.Mapped[int] = db.mapped_column(db.Integer, nullable=False)
+    publication_figures_id: db.Mapped[int] = db.mapped_column(
+        ForeignKey("publication_figures.id", ondelete="CASCADE"), nullable=False
+    )
+    img_name: db.Mapped[str] = db.mapped_column(db.String(64), nullable=False)
+    caption: db.Mapped[str] = db.mapped_column(db.Text, nullable=True)
+    img_url: db.Mapped[str] = db.mapped_column(db.String(256), nullable=True)
--- api/resources/gaia.py
+++ api/resources/gaia.py
@@ -1,12 +1,40 @@
-from flask_restx import Namespace, Resource
+from flask import request
+from flask_restx import Namespace, Resource, fields
 from markupsafe import escape
 from api import db
 from api.utils.bar_utils import BARUtils
-from api.models.gaia import Genes, Aliases
+from api.models.gaia import Genes, Aliases, PubIds, Figures
 from sqlalchemy import func, or_
+from marshmallow import Schema, ValidationError, fields as marshmallow_fields
 import json
 
 gaia = Namespace("Gaia", description="Gaia", path="/gaia")
+
+parser = gaia.parser()
+parser.add_argument(
+    "terms",
+    type=list,
+    action="append",
+    required=True,
+    help="Publication IDs",
+    default=["32492426", "32550561"],
+)
+
+publication_request_fields = gaia.model(
+    "Publications",
+    {
+        "pubmeds": fields.List(
+            required=True,
+            example=["32492426", "32550561"],
+            cls_or_instance=fields.String,
+        ),
+    },
+)
+
+
+# Validation is done in a different way to keep things simple
+class PublicationSchema(Schema):
+    pubmeds = marshmallow_fields.List(cls_or_instance=marshmallow_fields.String)
 
 
 @gaia.route("/aliases/<string:identifier>")
@@ -78,3 +106,54 @@ class GaiaAliases(Resource):
 
         else:
             return BARUtils.error_exit("Invalid identifier"), 400
+
+
+@gaia.route("/publication_figures")
+class GaiaPublicationFigures(Resource):
+    @gaia.expect(publication_request_fields)
+    def post(self):
+        json_data = request.get_json()
+        data = {}
+
+        # Validate json
+        try:
+            json_data = PublicationSchema().load(json_data)
+        except ValidationError as err:
+            return BARUtils.error_exit(err.messages), 400
+
+        pubmeds = json_data["pubmeds"]
+
+        # Check if pubmed ids are valide
+        for pubmed in pubmeds:
+            if not BARUtils.is_integer(pubmed):
+                return BARUtils.error_exit("Invalid Pubmed ID"), 400
+
+        # It is valid. Continue
+        data = []
+
+        # Left join is important in case aliases do not exist for the given locus / geneid
+        query = (
+            db.select(Figures.img_name, Figures.caption, Figures.img_url, PubIds.pubmed, PubIds.pmc)
+            .select_from(Figures)
+            .join(PubIds, PubIds.publication_figures_id == Figures.publication_figures_id)
+            .filter(PubIds.pubmed.in_(pubmeds))
+        )
+
+        rows = db.session.execute(query).fetchall()
+
+        # Just output the rows for now, we will format later
+        if rows and len(rows) > 0:
+            for row in rows:
+                record = {
+                    "img_name": row.img_name,
+                    "caption": row.caption,
+                    "img_url": row.img_url,
+                    "pubmed": row.pubmed,
+                    "pmc": row.pmc,
+                }
+
+                # Add the record to data
+                data.append(record)
+
+        # Return final data
+        return BARUtils.success_exit(data)
